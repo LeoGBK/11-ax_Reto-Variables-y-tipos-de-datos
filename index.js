582961615f6febe51dc25937ae1874a2521576fe
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-10-26 10:11:44 +0200

    Creating new variable for string in order to apply next line of code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let saludo = "¡Hola Mundo!";
 let nombre = "Juan";
 console.log("¡Hola Mundo! Soy", nombre);
 console.log("Mi nombre en mayúsculas es:", nombre.toUpperCase());
+console.log("Mi nombre en minúsculas es:", nombre.toLowerCase());
