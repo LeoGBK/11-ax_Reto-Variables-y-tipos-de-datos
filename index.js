65c6691f3f40e833bc8340b4f52fe97e9d4b3719
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-10-26 10:14:45 +0200

    Fixing minor error

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let nombre = "Juan";
 console.log("¡Hola Mundo! Soy", nombre);
 console.log("Mi nombre en mayúsculas es:", nombre.toUpperCase());
 console.log("Mi nombre en minúsculas es:", nombre.toLowerCase());
+console.log("El mensaje de", saludo, "tiene", saludo.length, "caracteres.");
