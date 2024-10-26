3a4c35168f2f05e0a03c1ff23ac16392255cf423
Leo Bermeo Kittyle <leobermeokittyle@gmail.com>
2024-10-26 10:16:26 +0200

    Creating a list for the next line of code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let saludo = "¡Hola Mundo!";
 let nombre = "Juan";
+let array = [1,2,3,4,5];
 console.log("¡Hola Mundo! Soy", nombre);
 console.log("Mi nombre en mayúsculas es:", nombre.toUpperCase());
 console.log("Mi nombre en minúsculas es:", nombre.toLowerCase());
